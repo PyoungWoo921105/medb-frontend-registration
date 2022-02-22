34200bbaa434515c136ef1cb09cda1d257640d4e
Pyoungwoo <pyoungwoo921105@gmail.com>
2022-02-22 19:00:20 +0900

    [feat] 병원 주소 지표 기입 서비스 구현

---
+++ src/services/hospital/GetHospitalCoordinate.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import UseStore from "../../data/useStore";
+
+export const GetHospitalCoordinate = async (data: any) => {
+  const { address } = data;
+  const HospitalData = UseStore().HospitalData;
+  try {
+    /* axios.get(url[, config]) */
+    axios.defaults.headers.common["Authorization"] = `KakaoAK ccb19825dd8f57563382211962178102`;
+    const response = await axios.get(`https://dapi.kakao.com/v2/local/search/address.json?query=${address}`);
+    HospitalData.setBusinessCurrentLocationData({
+      latitude: response.data.documents[0].y,
+      longitude: response.data.documents[0].x,
+    });
+    return response;
+  } catch (error: any) {
+    return error;
+  }
+};
