0b3a802cdc8bbb6d70bfe94c3e92ff8e64b708b8
Pyoungwoo <pyoungwoo921105@gmail.com>
2022-02-22 19:00:38 +0900

    [feat] 병원, 약국, 의사 데이터 타입 연동 추가

--- src/data/useStore.tsx
+++ src/data/useStore.tsx
@@ -1,8 +1,14 @@
 import { CommonData } from "./stores/CommonData";
+import { HospitalData } from "./stores/HospitalData";
+import { PharmacyData } from "./stores/PharmacyData";
+import { DoctorData } from "./stores/DoctorData";
 
 export const useStore = () => {
   return {
     CommonData,
+    HospitalData,
+    PharmacyData,
+    DoctorData,
   };
 };
 
