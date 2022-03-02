6514244a84a8e35ab54d069225f88de9674e1be9
Pyoungwoo <pyoungwoo921105@gmail.com>
2022-03-02 12:22:49 +0900

    [fix] 아이디 제약조건에 대한 기획 내용 수정사항 반영

--- src/components/join/pharmacy/PharmacyJoinThirdStepPage.tsx
+++ src/components/join/pharmacy/PharmacyJoinThirdStepPage.tsx
@@ -377,7 +377,7 @@ const PharmacyJoinThirdStepPage = observer((props: any) => {
   const [accountPasswordValidate, setAccountPasswordValidate] = useState(false);
 
   useEffect(() => {
-    const accountIDDataValidateFlagData = /^(?=.*[a-z])(?=.*[0-9])(?=.{6,20})/;
+    const accountIDDataValidateFlagData = /^(?=.+[a-z])(?=.*[0-9])(?=.{6,20})/;
     if (
       PharmacyData.accountIDData &&
       PharmacyData.accountIDData.match(accountIDDataValidateFlagData) &&
